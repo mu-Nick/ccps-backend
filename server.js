6684a68dae606a324d8eb74b275866f1a46c0787
Anubhav Singh <anubhavsigh2690@gmail.com>
2019-09-22 12:58:05 +0530

    setting up server

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt-nodejs");
+const cors = require("cors");
+const knex = require("knex");
+
+var knex = require("knex")({
+	client: "mysql",
+	connection: {
+		host: "127.0.0.1",
+		user: "anubhav",
+		password: "Abcd@1234",
+		database: "ccps"
+	}
+});
 
 const app = express();
+app.use(bodyParser.json());
+app.use(cors());
 
 app.listen(3000, () => {
 	console.log("app is running on port 3000");
